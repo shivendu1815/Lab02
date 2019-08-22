60929c5d7f1bce8ee6375a64d9903e38084b4d89
SHIVENDU <shivendu.sharma_cs18@gla.ac.in>
2019-08-22 15:02:21 +0530

    create main method

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,14 +1,11 @@
 package main;
 
-import java.util.Scanner;
-
 /**
  * @author shivendu
  * @version 1.0
  **/
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
 
     }
 
