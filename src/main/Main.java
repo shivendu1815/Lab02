2a8077b283512d8debedb594b33be2e98ffe214e
SHIVENDU <shivendu.sharma_cs18@gla.ac.in>
2019-08-22 15:13:58 +0530

    create object of scanner class

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,9 +4,14 @@ package main;
  * @author shivendu
  * @version 1.0
  **/
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         String userName = "";
+        Scanner scanner = new Scanner(System.in);
+
+
 
 
     }
